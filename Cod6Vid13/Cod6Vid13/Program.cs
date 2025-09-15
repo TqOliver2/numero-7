a7060f0c83217fc0f795c0a16c3b4dea42655d51
TqOliver2 <oliver.gonzalez6@utp.ac.pa>
2025-09-14 20:46:16 -0500

    Subo los codigos correspondientes del dia 15 de septiembre

---
+++ Cod6Vid13/Cod6Vid13/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+int  totPlayers = 15;
+int totDealer = 15;
+String message = "";
+//juntar 21 pidiendo cartas o en caso de tener menos
+//de 21 igual tener mayor puntacion que la maquina
+
+if (totPlayers > totDealer && totPlayers < 22)
+{
+    message = "Venciste al dealer, felicidades";
+} else if (totPlayers < totDealer) {
+    message = "Perdiste ";
+} else if (totPlayers > 21)
+{
+    message = "Perdiste, te pasaste de 21";
+}
+else
+{
+    message = "Condicion no valida";
+}
+Console.WriteLine(message);
---
+++ Cod7Vid14/Cod7Vid14/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+int totPlayers = 0;
+int totDealer = 0;
+String message = "";
+String switckcontrol = "menu";
+int num = 0;
+//juntar 21 pidiendo cartas o en caso de tener menos
+//de 21 igual tener mayor puntacion que la maquina
+while (true)
+{
+    switch (switckcontrol)
+    {
+        case "menu":
+            Console.WriteLine("Bienvenido al Casino");
+            Console.WriteLine("Escriba 21 pra jugar al 21");
+            switckcontrol = Console.ReadLine();
+            break;
+        case "21":
+
+            do
+            {
+                System.Random random = new System.Random();
+                num = random.Next(1, 12);
+                totPlayers = totPlayers + num;
+                Console.WriteLine("Toma tu carta...");
+                Console.WriteLine("Tu Carta es: " + num);
+                Console.WriteLine("Deseas otra carta? ");
+            } while (Console.ReadLine() == "si" || Console.ReadLine() == "Si");
+
+            if (totPlayers > totDealer && totPlayers < 22)
+            {
+                message = "Venciste al dealer, felicidades";
+                switckcontrol = "menu";
+
+            }
+            else if (totPlayers < totDealer)
+            {
+                message = "Perdiste ";
+                switckcontrol = "menu";
+            }
+            else if (totPlayers > 21)
+            {
+                message = "Perdiste, te pasaste de 21";
+                switckcontrol = "menu";
+            }
+            else
+            {
+                message = "Condicion no valida";
+                switckcontrol = "menu";
+            }
+            Console.WriteLine(message);
+            break;
+        default:
+            Console.WriteLine("Opcion no valida");
+            switckcontrol = "menu";
+            break;
+        case "exit":
+            break;
+    }
+
+
+}
+
