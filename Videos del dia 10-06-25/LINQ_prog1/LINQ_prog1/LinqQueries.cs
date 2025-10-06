2e7f1f83915b1de247f0a139b87576a38b36e99b
TqOliver2 <oliver.gonzalez6@utp.ac.pa>
2025-10-06 10:39:13 -0500

    Subo los videos correspondientes al dia 10-06

---
+++ Videos del dia 10-06-25/LINQ_prog1/LINQ_prog1/LinqQueries.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace LINQ_prog1
+{
+    internal class LinqQueries
+    {
+        private List<Libro> librosCollection = new List<Libro>();
+        public LinqQueries()
+        {
+            using (StreamReader reader = new StreamReader("books.json"))
+            {
+                string json = reader.ReadToEnd();
+                this.librosCollection = System.Text.Json.JsonSerializer.Deserialize<List<Libro>>(json, new System.Text.Json.JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+            }
+        }
+
+        public IEnumerable<Libro> TodaLaCollection()
+        {
+            return librosCollection;
+        }
+
+
+        public IEnumerable<Libro> booskBeforeOf2000()
+        {
+            return librosCollection.Where(p => p.PublishedDate.Year > 2000);
+        }
+
+
+        public IEnumerable<Libro> booksBeforeOf200QryExp()
+        {
+            return from l in librosCollection where l.PublishedDate.Year > 2000 select l;
+        }
+
+        public IEnumerable<Libro> books250pagANDtileAction()
+        {
+            return from l in librosCollection where l.PageCount > 250 && l.Title.Contains("in Action") select l;
+        }
+
+        public bool UseOfOperatorALL()
+        {
+            return librosCollection.All(p => p.Status != string.Empty);
+        }
+
+        public bool UseOfOperatorANY()
+        {
+            return librosCollection.Any(p => p.PublishedDate.Year == 2005);
+        }
+
+        public IEnumerable<Libro> booksCatPython()
+        {
+            return librosCollection.Where(p => p.Categories.Contains("Python"));
+        }
+
+        public IEnumerable<Libro> booksCatJavaOrderByName()
+        {
+            return librosCollection.Where(p => p.Categories.Contains("Java")).OrderBy(p => p.Title);
+        }
+
+        public IEnumerable<Libro> booksMore450PagOrderByDes()
+        {
+            return librosCollection.Where(p => p.PageCount > 450).OrderByDescending(p => p.PageCount);
+        }
+
+        public IEnumerable<Libro> threeBooksJavaOrderByDate()
+        {
+            return librosCollection
+                .Where(p => p.Categories.Contains("Java"))
+                .OrderBy(p => p.PublishedDate)
+                .TakeLast(3);
+        }
+
+
+        public IEnumerable<Libro> thirdAndFourthBooksMore400pages()
+        {
+            return librosCollection
+                .Where(p => p.PageCount > 400)
+                .Take(4)
+                .Skip(2);
+        }
+
+
+        public IEnumerable<Libro> ThreeBooksInCollection()
+        {
+            return librosCollection
+                .Take(3)
+                .Select(p => new Libro() { Title = p.Title, PageCount = p.PageCount });//Se crea una nueva coleccion solo con dos datos
+        }
+    }
+}
---
+++ Videos del dia 10-06-25/LINQ_prog1/LINQ_prog1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.NetworkInformation;
+using LINQ_prog1;
+
+LinqQueries queries = new LinqQueries();
+
+void printfValues(IEnumerable<Libro> listBooks)
+{
+    
+    Console.WriteLine("{0, -60} {1, 15} {2, 15}\n ", "Titulo", "N. Paginas", "Fecha de publicacion");
+    foreach (var item in listBooks)
+    {
+        Console.WriteLine("{0, -60} {1, 15} {2, 15}", item.Title, item.PageCount, item.PublishedDate.ToShortDateString());
+    }
+}
+
+
+Console.WriteLine("------- TODOS LOS LIBROS");
+printfValues(queries.TodaLaCollection());
+Console.WriteLine("\n\n\n\n\n");
+
+Console.WriteLine("------- LIBROS PUBLICADOS DESPUES DEL 2000");
+printfValues(queries.booskBeforeOf2000());
+Console.WriteLine("\n\n\n\n\n");
+
+Console.WriteLine("------- USO DE QUERY EXPRESION");
+printfValues(queries.booksBeforeOf200QryExp());
+Console.WriteLine("\n\n\n\n\n");
+Console.WriteLine("------- LIBROS CON 250 PAGINAS Y QUE CONTENGA ACTION");
+printfValues(queries.books250pagANDtileAction());
+Console.WriteLine("\n\n\n\n\n");
+Console.WriteLine("------- USO DE ALL");
+Console.WriteLine("Todos los libros tiene status?: " + queries.UseOfOperatorALL());
+Console.WriteLine("\n\n\n\n\n");
+
+Console.WriteLine("------- USO DE ANY");
+Console.WriteLine("Algun libro fue publicado en el 2005?: " + queries.UseOfOperatorANY());
+Console.WriteLine("\n\n\n\n\n");
+
+
+Console.WriteLine("------- USO DE CONTAINS");
+printfValues(queries.booksCatPython());
+Console.WriteLine("\n\n\n\n\n");
+
+Console.WriteLine("------- USO DE ORDER BY");
+printfValues(queries.booksCatJavaOrderByName());
+Console.WriteLine("\n\n\n\n\n");
+
+Console.WriteLine("------- USO DE ORDER BY DECENDING");
+printfValues(queries.booksMore450PagOrderByDes());
+Console.WriteLine("\n\n\n\n\n");
+
+
+Console.WriteLine("------- USO DE TAKE");
+printfValues(queries.threeBooksJavaOrderByDate());
+Console.WriteLine("\n\n\n\n\n");
+
+Console.WriteLine("------- USO DE SKIP");
+printfValues(queries.thirdAndFourthBooksMore400pages());
+Console.WriteLine("\n\n\n\n\n");
+
+Console.WriteLine("------- USO DE SELECCION DINAMICA");
+printfValues(queries.ThreeBooksInCollection());
+Console.WriteLine("\n\n\n\n\n");
